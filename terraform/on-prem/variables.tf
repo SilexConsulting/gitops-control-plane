37ebd5acc5bb38968da0692506d8aa39eea4a0a2
Noel Sharpe <noels@radnetwork.co.uk>
2026-09-25 18:34:33 +0100

    GIT-38: allow enable_external_dns (allowed_addons in on-prem, hub, spokes)

--- terraform/on-prem/variables.tf
+++ terraform/on-prem/variables.tf
@@ -86,7 +86,7 @@ variable "addons" {
 variable "allowed_addons" {
   description = "Optional allowlist of known addon flags."
   type        = list(string)
-  default     = ["argocd", "keycloak", "velero", "cnpg", "mariadb_operator", "metallb", "external_secrets", "cert_manager", "longhorn"]
+  default     = ["argocd", "keycloak", "velero", "cnpg", "mariadb_operator", "metallb", "external_secrets", "cert_manager", "longhorn", "external_dns"]
 }
 
 variable "distro_provided_addons" {
